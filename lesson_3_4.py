8d9f30dccde08c816657d20b6cf8b2ded31c893d
alvekimov <al.v.ekimov@gmail.com>
2020-09-17 22:41:49 +0500

    update file lesson_3_3

--- lesson_3_4.py
+++ lesson_3_4.py
@@ -3,10 +3,12 @@
 def func_pow(x, y):
     """"Программа принимает действительное положительное число x и целое отрицательное число y.
     Необходимо выполнить возведение числа x в степень y"""
-    x_in_pow_y = 1
-    for i in range(abs(round(y))):
-        x_in_pow_y = x_in_pow_y * abs(x)
-    return round((1 / x_in_pow_y), 5)
-
+    if y < 0:
+        x_in_pow_y = 1
+        for i in range(abs(round(y))):
+            x_in_pow_y = x_in_pow_y * abs(x)
+        return round((1 / x_in_pow_y), 5)
+    else:
+        return "Y is not less than zero"
 
 print(func_pow(21, -3))
